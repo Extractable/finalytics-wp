e18654ad4210feef40222f369b570f9cbace2a64
alyona.danilina <alyona.danilina@scotts.com>
2022-08-25 14:27:57 +0300

    [fwd-30/platform-page] Quote section (front)

--- wp-content/themes/finalytics/template-parts/acf-blog-preview.php
+++ wp-content/themes/finalytics/template-parts/acf-blog-preview.php
@@ -13,28 +13,28 @@
 <?php if (get_sub_field("display_blog_preview")): ?>
   <section <?php if (!empty($blog_preview_custom_id)): ?>id="<?php echo $blog_preview_custom_id; ?>"<?php endif; ?>
            class="blog-preview <?php if (get_sub_field('blog_preview_color_scheme') == 'dark_blue'): ?>scheme_dark-blue<?php
-                                    elseif (get_sub_field('blog_preview_color_scheme') == 'white'): ?>scheme_white<?php
-                                    elseif (get_sub_field('blog_preview_color_scheme') == 'light_blue'): ?>scheme_light-blue<?php endif; ?>">
+           elseif (get_sub_field('blog_preview_color_scheme') == 'white'): ?>scheme_white<?php
+           elseif (get_sub_field('blog_preview_color_scheme') == 'light_blue'): ?>scheme_light-blue<?php endif; ?>">
     <div class="decor-element"></div>
-
     <div class="container">
       <div class="blog-preview_content">
         <h2 class="h2 blog-preview_title"><?php if (get_sub_field("blog_preview_title")) echo get_sub_field("blog_preview_title"); ?></h2>
         <p class="blog-preview_text"><?php if (get_sub_field("blog_preview_text")) echo get_sub_field("blog_preview_text"); ?></p>
         <div class="blog-preview_list">
           <?php $blog_preview_posts = get_sub_field('blog_preview_posts');
-          if( $blog_preview_posts ): ?>
+          if ($blog_preview_posts): ?>
             <div class="row">
-              <?php foreach( $blog_preview_posts as $post ):
-
+              <?php foreach ($blog_preview_posts as $post):
                 // Setup this post for WP functions (variable must be named $post).
                 setup_postdata($post); ?>
                 <div class="col-md-4">
                   <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="blog-preview_item-link">
                     <div class="blog-preview_item-thumb"><?php the_post_thumbnail(); ?></div>
                     <div class="blog-preview_item-content">
-                      <span class="blog-preview_item-eyebrow"><?php if (get_sub_field("blog_post_eyebrow")) echo get_sub_field("blog_post_eyebrow"); ?></span>
-                      <h4 class="h4 blog-preview_item-title"><?php $title = the_title('','',FALSE); echo substr($title, 0, 27); ?></h4>
+                      <span
+                          class="blog-preview_item-eyebrow"><?php if (get_sub_field("blog_post_eyebrow")) echo get_sub_field("blog_post_eyebrow"); ?></span>
+                      <h4 class="h4 blog-preview_item-title"><?php $title = the_title('', '', FALSE);
+                        echo substr($title, 0, 27); ?></h4>
                       <div class="blog-preview_item-excerpt"><?php the_excerpt(); ?></div>
                     </div>
                   </a>
@@ -46,10 +46,9 @@
             wp_reset_postdata(); ?>
           <?php endif; ?>
         </div>
-
         <?php
         $blog_preview_link = get_sub_field('blog_preview_link');
-        if( $blog_preview_link ):
+        if ($blog_preview_link):
           $link_url = $blog_preview_link['url'];
           $link_title = $blog_preview_link['title'];
           $link_target = $blog_preview_link['target'] ? $blog_preview_link['target'] : '_self';
@@ -57,14 +56,12 @@
           <div class="blog-preview_cta">
             <a class="btn btn-link <?php if (get_sub_field('blog_preview_color_scheme') == 'light_blue'): ?>btn-link__blue<?php
             elseif (get_sub_field('blog_preview_color_scheme') == 'white'): ?>btn-link__blue<?php endif; ?>"
-               href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"
-               title="<?php echo esc_html( $link_title ); ?>"><span
-                  class="btn-link_text"><?php echo esc_html( $link_title ); ?></span></a>
+               href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
+               title="<?php echo esc_html($link_title); ?>"><span
+                  class="btn-link_text"><?php echo esc_html($link_title); ?></span></a>
           </div>
         <?php endif; ?>
-
       </div>
     </div>
-
   </section>
 <?php endif; ?>
--- wp-content/themes/finalytics/template-parts/acf-quote.php
+++ wp-content/themes/finalytics/template-parts/acf-quote.php
@@ -9,75 +9,30 @@
 
 ?>
 
-<?php $promo_callout_custom_id = get_sub_field("promo_callout_custom_id"); ?>
-<section <?php if (!empty($promo_callout_custom_id)): ?>id="<?php echo $promo_callout_custom_id; ?>"<?php endif; ?>
-         class="promo-callout <?php if (get_sub_field('promo_callout_color_scheme') == 'white'): ?>scheme_white<?php
-         elseif (get_sub_field('promo_callout_color_scheme') == 'dark_blue'): ?>scheme_dark-blue<?php
-         elseif (get_sub_field('promo_callout_color_scheme') == 'light_gray'): ?>scheme_light-gray<?php
-         elseif (get_sub_field('promo_callout_color_scheme') == 'light_blue'): ?>scheme_light_blue<?php
-         endif; ?> <?php if (get_sub_field('promo_callout_image_position') == 'right'): ?>image-right<?php else: ?>image-left<?php endif; ?>">
-  <div class="promo-callout_wrapper">
-    <div class="container">
-      <h2 class="h2 promo-callout_heading"><?php if (get_sub_field("promo_callout_heading")) echo get_sub_field("promo_callout_heading"); ?></h2>
-      <div class="row">
-        <div class="col-md-6 col-lg-8">
-          <div class="promo-callout_image">
-            <?php
-            $promo_callout_image = get_sub_field('promo_callout_image');
-            if (!empty($promo_callout_image)): ?>
-              <img src="<?php echo $promo_callout_image['url']; ?>"
-                   alt="<?php echo $promo_callout_image['alt']; ?>" />
-            <?php endif; ?>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="promo-callout_content-wrapper">
-            <div class="promo-callout_content">
-              <h3 class="h3-secondary promo-callout_title">
-                <?php if (get_sub_field("promo_callout_title")) echo get_sub_field("promo_callout_title"); ?>
-              </h3>
-              <p class="promo-callout_text"><?php if (get_sub_field("promo_callout_text")) echo get_sub_field("promo_callout_text"); ?></p>
-              <div class="promo-callout_cta">
-                <?php
-                $promo_callout_button_1 = get_sub_field('promo_callout_button_1');
-                if( $promo_callout_button_1 ):
-                  $link_url = $promo_callout_button_1['url'];
-                  $link_title = $promo_callout_button_1['title'];
-                  ?>
-
-                  <a class="btn btn-link <?php
-                  if (get_sub_field('promo_callout_color_scheme') == 'light_blue'): ?>btn-link__blue<?php
-                  elseif (get_sub_field('promo_callout_color_scheme') == 'white'): ?>btn-link__blue<?php
-                  elseif (get_sub_field('promo_callout_color_scheme') == 'light_gray'): ?>btn-link__blue
-                    <?php endif; ?>"
-                     href="<?php echo esc_url( $link_url ); ?>"
-                     title="<?php echo esc_html( $link_title ); ?>"><span
-                        class="btn-link_text"><?php echo esc_html( $link_title ); ?></span></a>
-
-                <?php endif; ?>
-                <?php
-                $promo_callout_button_2 = get_sub_field('promo_callout_button_2');
-                if( $promo_callout_button_2 ):
-                  $link_url = $promo_callout_button_2['url'];
-                  $link_title = $promo_callout_button_2['title'];
-                  ?>
-
-                  <a class="btn btn-link <?php
-                  if (get_sub_field('promo_callout_color_scheme') == 'light_blue'): ?>btn-link__blue<?php
-                  elseif (get_sub_field('promo_callout_color_scheme') == 'white'): ?>btn-link__blue<?php
-                  elseif (get_sub_field('promo_callout_color_scheme') == 'light_gray'): ?>btn-link__blue
-                    <?php endif; ?>"
-                     href="<?php echo esc_url( $link_url ); ?>"
-                     title="<?php echo esc_html( $link_title ); ?>"><span
-                        class="btn-link_text"><?php echo esc_html( $link_title ); ?></span></a>
-
-                <?php endif; ?>
-              </div>
-            </div>
-          </div>
-        </div>
+<?php $quote_custom_id = get_sub_field("quote_custom_id"); ?>
+<section <?php if (!empty($quote_custom_id)): ?>id="<?php echo $quote_custom_id; ?>"<?php endif; ?>
+         class="quote <?php if (get_sub_field('quote_color_scheme') == 'white'): ?>scheme_white<?php
+         elseif (get_sub_field('quote_color_scheme') == 'dark_blue'): ?>scheme_dark-blue<?php endif; ?>
+         <?php if (get_sub_field("hide_overflow")): ?>overflow-hidden<?php endif; ?>">
+  <div class="container-fluid">
+    <blockquote
+        class="quote_text"><?php if (get_sub_field("quote_text")) echo get_sub_field("quote_text"); ?></blockquote>
+    <cite class="quote_author">
+      <div class="quote_photo">
+        <?php
+        $quote_author_photo = get_sub_field('quote_author_photo');
+        if (!empty($quote_author_photo)): ?>
+          <img src="<?php echo $quote_author_photo['url']; ?>"
+               alt="<?php echo $quote_author_photo['alt']; ?>"/>
+        <?php endif; ?>
       </div>
-    </div>
+      <div class="quote_author-info">
+        <span
+            class="quote_author-name"><?php if (get_sub_field("quote_author_name")) echo get_sub_field("quote_author_name"); ?></span>
+        <span
+            class="quote_author-position"><?php if (get_sub_field("quote_author_position")) echo get_sub_field("quote_author_position"); ?></span>
+      </div>
+    </cite>
   </div>
   <div class="decor-element"></div>
 </section>
