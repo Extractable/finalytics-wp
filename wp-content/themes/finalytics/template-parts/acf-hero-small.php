01742f451b5b266f9f3c1cf997c933438f76e8ad
alyona.danilina <alyona.danilina@scotts.com>
2022-09-07 16:05:01 +0300

    [fwd-34/insights-archive] Update hero-small banner

--- wp-content/themes/finalytics/template-parts/acf-hero-small.php
+++ wp-content/themes/finalytics/template-parts/acf-hero-small.php
@@ -10,15 +10,19 @@
 ?>
 <?php $hero_small_custom_id = get_sub_field("hero_small_custom_id"); ?>
 <section <?php if (!empty($hero_small_custom_id)): ?>id="<?php echo $hero_small_custom_id; ?>"<?php endif; ?>
-         class="hero-small <?php if( get_sub_field('hero_small_color_scheme') == 'light' ): ?>scheme-light<?php else: ?>scheme-dark<?php endif; ?> <?php
-         if (get_sub_field("hero_small_hide_overflow")): ?>overflow-hidden<?php endif; ?>">
+         class="hero-small <?php if (get_sub_field('hero_small_color_scheme') == 'light'): ?>scheme-light<?php else: ?>scheme-dark<?php endif; ?> <?php
+         if (get_sub_field("hero_small_hide_overflow")): ?>overflow-hidden<?php endif; ?> <?php if (get_sub_field('hero_small_background_image') == 'waves'): ?>bg-waves<?php
+         else: ?>bg-dotted<?php endif; ?> <?php if (get_sub_field('hero_small_banner_height') == 'medium'): ?>height-medium<?php
+         else: ?>height-small<?php endif; ?>">
   <div class="container-xl">
-
     <div class="hero-small_content">
-        <span
-            class="hero-small_eyebrow"><?php if (get_sub_field("hero_small_eyebrow")) echo get_sub_field("hero_small_eyebrow"); ?></span>
+      <?php if (!empty(get_sub_field("hero_small_eyebrow"))): ?>
+        <span class="hero-small_eyebrow"><?php echo get_sub_field("hero_small_eyebrow"); ?></span>
+      <?php endif; ?>
       <h1 class="h1-small hero-small_title"><?php if (get_sub_field("hero_small_title")) echo get_sub_field("hero_small_title"); ?></h1>
-      <p class="hero-small_text"><?php if (get_sub_field("hero_small_text")) echo get_sub_field("hero_small_text"); ?></p>
+      <?php if (!empty(get_sub_field("hero_small_text"))): ?>
+        <p class="hero-small_text"><?php echo get_sub_field("hero_small_text"); ?></p>
+      <?php endif; ?>
       <div class="hero-small_cta">
         <?php
         $hero_small_button_1 = get_sub_field('hero_small_button_1');
@@ -30,14 +34,13 @@
           <a class="btn btn-primary"
              href="<?php echo esc_url($link_url); ?>"
              title="<?php echo esc_html($link_title); ?>"><?php echo esc_html($link_title); ?></a>
-        <?php else: ?>
+          <?php else: ?>
           <a class="btn btn-link <?php if (get_sub_field('hero_small_color_scheme') == 'light'): ?>btn-link__blue<?php endif; ?>"
              href="<?php echo esc_url($link_url); ?>"
              title="<?php echo esc_html($link_title); ?>"><span
                 class="btn-link_text"><?php echo esc_html($link_title); ?></span></a>
+          <?php endif; ?>
         <?php endif; ?>
-        <?php endif; ?>
-
         <?php
         $hero_small_button_2 = get_sub_field('hero_small_button_2');
         if ($hero_small_button_2):
@@ -48,17 +51,16 @@
           <a class="btn btn-primary"
              href="<?php echo esc_url($link_url); ?>"
              title="<?php echo esc_html($link_title); ?>"><?php echo esc_html($link_title); ?></a>
-        <?php else: ?>
+          <?php else: ?>
           <a class="btn btn-link <?php if (get_sub_field('hero_small_color_scheme') == 'light'): ?>btn-link__blue<?php endif; ?>"
              href="<?php echo esc_url($link_url); ?>"
              title="<?php echo esc_html($link_title); ?>"><span
                 class="btn-link_text"><?php echo esc_html($link_title); ?></span></a>
-        <?php endif; ?>
+          <?php endif; ?>
         <?php endif; ?>
       </div>
     </div>
   </div>
-
 
   <div class="decor-element"></div>
 </section>
